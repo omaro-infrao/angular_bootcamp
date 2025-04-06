138d341790cca08155c7a3737c298ad450ba3f80
Omaro <zimomar76@hotmail.fr>
2025-04-06 15:35:14 +0200

    showing products, cart management still to be done

--- app.js
+++ app.js
@@ -1,14 +1,34 @@
-var app = angular.module("contactApp", []);
-app.controller('inputCtrl', function($scope){
-    // Initialize contacts from localStorage or empty array if none exists
-    $scope.contacts = JSON.parse(localStorage.getItem('contacts')) || [];
-    $scope.firstNameInput = '';
-    $scope.lastNameInput = '';
-    $scope.emailInput = '';
-    $scope.phoneInput = '';
-    $scope.searchInput = '';
-    $scope.sortDirection = false; // false for ascending, true for descending
+var app = angular.module("catalogApp", []);
 
+// Ajout du filtre unique
+app.filter('unique', function() {
+    return function(collection, keyname) {
+        var output = [],
+            keys = [];
+
+        angular.forEach(collection, function(item) {
+            var key = item[keyname];
+            if(keys.indexOf(key) === -1) {
+                keys.push(key);
+                output.push(item);
+            }
+        });
+        return output;
+    };
+});
+
+app.controller('mainCtrl', function($scope){
+
+    $scope.selectedCategory = "";
+    
+    $scope.products = [
+        { id: 1, name: "Pomme", price: 0.5, category: "Fruits", description: "Une belle pomme rouge" },
+        { id: 2, name: "Coca-Cola", price: 1.2, category: "Boissons", description: "Canette de 33cl" },
+        { id: 3, name: "Clé USB 16Go", price: 8.99, category: "Électronique", description: "USB 3.0 rapide" }
+      ];
+
+    $scope.cart = [];
+      
     // Function to save contacts to localStorage
     function saveToLocalStorage() {
         localStorage.setItem('contacts', JSON.stringify($scope.contacts));
@@ -18,58 +38,8 @@ app.controller('inputCtrl', function($scope){
     function generateId() {
         return Date.now().toString(36) + Math.random().toString(36).substr(2);
     }
-
-    // Function to toggle sort direction
-    $scope.toggleSortDirection = function() {
-        $scope.sortDirection = !$scope.sortDirection;
-        console.log('Sort direction changed to:', $scope.sortDirection);
-    };
-
-    $scope.handleSubmit = function(){
-        $scope.contacts.push({
-            id: generateId(), // Add unique ID
-            firstName: $scope.firstNameInput,
-            lastName: $scope.lastNameInput,
-            email: $scope.emailInput,
-            phone: $scope.phoneInput,
-            isEditing: false
-        });    
-        // Clear inputs
-        $scope.firstNameInput = '';
-        $scope.lastNameInput = '';
-        $scope.emailInput = '';
-        $scope.phoneInput = '';
-        
-        // Save to localStorage
-        saveToLocalStorage();
-    }
-
-    $scope.editContact = function(contact){
-        // Create a deep copy of the contact object
-        $scope.oldContact = JSON.parse(JSON.stringify(contact));
-        contact.isEditing = true;
-        console.log("OLD CONTACT", $scope.oldContact);
-    }
-
-    $scope.cancelEdit = function(contact){
-        console.log($scope.oldContact);
-        contact.firstName = $scope.oldContact.firstName;
-        contact.lastName = $scope.oldContact.lastName;
-        contact.email = $scope.oldContact.email;
-        contact.phone = $scope.oldContact.phone;
-        contact.isEditing = false;
-    }
-
-    $scope.deleteContact = function(contact){
-        var index = $scope.contacts.indexOf(contact);
-        if (index > -1) {
-            $scope.contacts.splice(index, 1);
-            saveToLocalStorage();
-        }
-    }
-
-    $scope.saveEdit = function(contact){
-        contact.isEditing = false;
-        saveToLocalStorage();
+    
+    $scope.addProduct = function(product) {
+        console.log('Produit ajouté:', product);
     }
 });
